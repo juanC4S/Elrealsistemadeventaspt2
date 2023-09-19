7a8f2503d06eed18bd9486bc817df93aafaa6c89
JC CS <juancarloscs2010@gmail.com>
2023-09-19 01:50:15 -0500

    agregue entidad categoria

---
+++ Sol_Minimarket.Datos/D_Categorias.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Sol_Minimarket.Entidades;
+
+namespace Sol_Minimarket.Datos
+{
+    public class D_Categorias
+    {
+        public DataTable Listado_ca (string cTexto)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SQLcon = new SqlConnection();
+
+            try
+            {
+                SQLcon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando= new SqlCommand("USP_Listado_ca", SQLcon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@cTexto", SqlDbType.VarChar).Value = cTexto;
+                SQLcon.Open();
+                Resultado=Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if(SQLcon.State==ConnectionState.Open) SQLcon.Close();
+            }
+        }
+    }
+}
